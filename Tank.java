a90398b092da2d8828e261a563565f5d446ca36f
allconnected <75664792+allconnected@users.noreply.github.com>
2020-12-08 10:28:05 +0100

    Base
    Base for programming the game

---
+++ Tank.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tank extends Actor
+{
+    /**
+     * Act - do whatever the Tank wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.isKeyDown("right")){
+            setLocation(getX() + 1, getY());
+        }
+        if(Greenfoot.isKeyDown("left")){
+            setLocation(getX() - 1, getY());
+        }
+        
+        if(Greenfoot.isKeyDown("space")){
+            Bullet bullet = new Bullet();
+            getWorld().addObject(bullet, getX(), 690);
+        }
+    }
+}
